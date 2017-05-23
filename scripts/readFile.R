cf7f73c6ed23714b8c4abb1f0e9156b2f04050fc
Liuyang Fu <liuyaf@uw.edu>
2017-05-22 21:43:16 -0700

    readFile.r changes

--- scripts/readFile.R
+++ scripts/readFile.R
@@ -6,3 +6,5 @@ can.wa.16.raw <- read.csv('./data/2016_WA_Candidates.csv', stringsAsFactors = FA
 can.wa.16 <- can.wa.16.raw %>% select(Candidate.id, Candidate, Election_Status, Status_of_Candidate,
                                       Specific_Party, General_Party, Office_Sought, Num_of_Records, Total_dollar)
 write.csv(can.wa.16, file = 'data/filtered_WA_can_16.csv', row.names = FALSE)
+
+
