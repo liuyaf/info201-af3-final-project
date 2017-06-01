e96ee7f98b49aad7cd27c7098fff32ee25b53d9d
Dean Barlan <Dean Barlan>
2017-05-31 18:35:19 -0700

    Changed hovertext again

--- scripts/ElectionPieChart.R
+++ scripts/ElectionPieChart.R
@@ -1,14 +1,14 @@
 library(shiny)
-library(dplyr)
 library(plotly)
 library(ggplot2)
+library(dplyr)
+
 
 BuildPieChart <- function(house.results.temp, min, max) {
   #filter the given data to abide by the slider data 
-  house.filtered <- house.results.temp %>% filter(Total_. > min & Total_. < max)
-  
+  house.filtered <- house.results.temp %>% filter(total >= min & total <= max)
   #Filter data to only show wins and loses, not pending or other info
-  final.house.data <- house.filtered %>% filter(Status_of_Candidate == "Won" | Status_of_Candidate == "Lost" )
+  final.house.data <- house.filtered %>% filter(Status_of_Candidate == "Won" || Status_of_Candidate == "Lost" )
   #count total members, total winners, and total losers to calculate the percentages
   total.rows <- sum(complete.cases(final.house.data))
   num.winners <- sum(final.house.data$Status_of_Candidate == "Won")
@@ -21,17 +21,12 @@ BuildPieChart <- function(house.results.temp, min, max) {
   percent <- num.winners / total.rows
 
   
-   pie.results <- plot_ly(data.graph, labels = ~Won.Lost, values = ~Percentages, type = 'pie',
-                         hoverinfo = 'text',
-                         text = ~paste0("Won/Lost = ", Won.Lost, " ,</br>", " Percent = ", round(Percentages, digits = 2)*100, "%,</br>",
-                                        "Number of Politicians = ", Results)) %>%
-     layout(width = 750, height = 750)
-
   #Create pie chart that displays the percentage that won the election and lost based on money raised, including a hover text option
-   pie.results <- plot_ly(data.graph, labels = ~Won.Lost, values = ~Percentages, type = 'pie', width = 300, height = 300,
+   pie.results <- plot_ly(data.graph, labels = ~Won.Lost, values = ~Percentages, type = 'pie',
                          hoverinfo = 'text', 
-                         text = ~paste0("Won/Lost = ", Won.Lost, " ,", " Percent = ", round(Percentages, digits = 2)*100, "%" )) %>%
-     layout(title = 'Politicians Election Results Based On Money Raised',
+                         text = ~paste0("Won/Lost = ", Won.Lost," ,</br>", " Percent = ", round(Percentages, digits = 2)*100, "%,</br>",
+                                        "Number of Politicians = ", Results)) %>%
+     layout(width = 750, height = 750,
             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
   
